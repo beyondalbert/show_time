67479d37b9fd314cafc4f876759149b984060299
beyondalbert <beyondalbert@gmail.com>
2017-05-20 21:37:22 +0800

    更新页面默认素材

--- app/models/basic_info.rb
+++ app/models/basic_info.rb
@@ -1,7 +1,7 @@
 class BasicInfo < ActiveRecord::Base
-	has_attached_file :logo_square, styles: { medium: "300x300>", thumb: "50x50>" }, default_url: "/images/:style/missing.png"
-	has_attached_file :logo_rectangle, styles: { medium: "150x50>", thumb: "25x75>" }, default_url: "/images/:style/missing.png"
-	has_attached_file :wx_qr_code, styles: { medium: "86x86>", thumb: "43x43>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :logo_square, styles: { medium: "50x50>", thumb: "20x20>" }, default_url: "logo.jpg"
+	has_attached_file :logo_rectangle, styles: { medium: "150x50>", thumb: "25x75>" }, default_url: "logo_rectangle.png"
+	has_attached_file :wx_qr_code, styles: { medium: "86x86>", thumb: "43x43>" }, default_url: "weixin.png"
 
 	validates_attachment_content_type :logo_square, content_type: /\Aimage\/.*\z/
 	validates_attachment_content_type :logo_rectangle, content_type: /\Aimage\/.*\z/
